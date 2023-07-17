225ef51908cdc4ecd3b19d430d907e389814763a
lakshankd <dominiclakshank@gmail.com>
2023-07-17 11:12:56 +0530

    Initialising project.

---
+++ task-management-system-backend/src/main/java/com/lakshankd/taskmanagementsystembackend/TaskManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lakshankd.taskmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemBackendApplication.class, args);
+	}
+
+}
