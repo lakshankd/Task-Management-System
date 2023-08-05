5a67dc8bbf5313752aa36c9cd7dcc73e7cad73ce
lakshankd <dominiclakshank@gmail.com>
2023-08-05 11:40:03 +0530

    updating user controller and service.

--- task-management-system-backend/src/main/java/com/lakshankd/taskmanagementsystem/controller/UserController.java
+++ task-management-system-backend/src/main/java/com/lakshankd/taskmanagementsystem/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.lakshankd.taskmanagementsystem.controller;
 
+import com.lakshankd.taskmanagementsystem.entity.User;
 import com.lakshankd.taskmanagementsystem.service.UserService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+    
 }
